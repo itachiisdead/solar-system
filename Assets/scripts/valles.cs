01f29e7e225de60a935b57e757cf60affcd80819
hebx <hebamaher0101@gmail.com>
2023-10-07 02:55:40 +0300

    added valles and olympus

---
+++ Assets/scripts/valles.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class valles : MonoBehaviour
+{
+    public void OnMouseUpAsButton()
+    {
+        SceneManager.LoadSceneAsync("valles");
+    }
+}
+
